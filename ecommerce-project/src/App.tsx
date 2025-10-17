356eccc5980885781ffb3c42b2b219ead2453f8f
ShopyK4 <1108564336@u.icesi.edu.co>
2025-10-16 20:54:33 -0500

    feat(ui) component sections fully implemented

--- ecommerce-project/src/App.tsx
+++ ecommerce-project/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Functionalities from './components/layout/Functionalities'
 import Navbar from './components/layout/Header'
 import LandingCarousel from './components/layout/LandingCarousel'
 import Sections from './components/layout/Sections'
@@ -7,10 +8,11 @@ function App() {
 
   return (
     <>  
-      <div className="pt-60 max-sm:pt-20 space-y-10 max-sm:space-y-8">
+      <div className="pt-60 max-sm:pt-20 space-y-20 max-sm:space-y-8">
         <Navbar />
         <LandingCarousel />
         <Sections />
+        <Functionalities />
       </div>
     </>
   )
---
+++ ecommerce-project/src/components/layout/Functionalities.tsx
@@ -0,0 +1,29 @@
+import viF from "../../../public/images/icons/Functionalities Vi.svg"
+import jinxF from "../../../public/images/icons/Functionalities Jinx.svg"
+import caitF from "../../../public/images/icons/Functionalities Cait.svg"
+import ekkoF from "../../../public/images/icons/Functionalities Ekko.svg"
+
+export default function Functionalities() {
+    return(
+        <>
+            <div className="relative flex items-center justify-center gap-5">
+                <div>
+                    <img src={viF} alt="" className="mx-auto block object-contain max-w-full
+                    max-sm:max-w-[180px] max-sm:max-h-[180px] transform transition-transform duration-200 ease-in-out hover:scale-105 focus-visible:scale-105 cursor-pointer"/>
+                </div>
+                <div className="mt-12">
+                    <img src={jinxF} alt="" className="mx-auto block object-contain max-w-full
+                    max-sm:max-w-[180px] max-sm:max-h-[180px] transform transition-transform duration-200 ease-in-out hover:scale-105 focus-visible:scale-105 cursor-pointer"/>
+                </div>
+                <div>
+                    <img src={caitF} alt="" className="mx-auto block object-contain max-w-full
+                    max-sm:max-w-[180px] max-sm:max-h-[180px] transform transition-transform duration-200 ease-in-out hover:scale-105 focus-visible:scale-105 cursor-pointer"/>
+                </div>
+                <div className="mt-12">
+                    <img src={ekkoF} alt="" className="mx-auto block object-contain max-w-full
+                    max-sm:max-w-[180px] max-sm:max-h-[180px] transform transition-transform duration-200 ease-in-out hover:scale-105 focus-visible:scale-105 cursor-pointer"/>
+                </div>
+            </div>
+        </>
+    )
+}
