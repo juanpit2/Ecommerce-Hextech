2a2bc83bc2c1ea7cb00eb81d3505e5a6bf5435e3
juanpit2 <juanpablo.ceballos05@gmail.com>
2025-11-07 11:09:36 -0500

    feat(ui): Logic of ProductPitlover page

--- ecommerce-project/src/components/layout/BannerPitlover.tsx
+++ ecommerce-project/src/components/layout/BannerPitlover.tsx
@@ -1,10 +1,9 @@
-import BannerPitlover from "../../../public/images/icons/BannerPitloverProduct.jpg"
 export default function BannerProducts() {
   return (
     <div className="w-full">
       <img
-        src= {BannerPitlover}
-        className="w-full h-[380px] object-cover rounded-xl"
+        src="public/images/icons/BannerPitloverProduct.jpg"
+        className="w-full py-[-200px] h-[380px] object-cover rounded-xl"
         alt="Products Banner"
       />
     </div>
--- ecommerce-project/src/main.tsx
+++ ecommerce-project/src/main.tsx
@@ -1,11 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import "./App.css";
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from './pages/Home.tsx'
-import LoginPage from './pages/LoginPage.tsx'
-import SignInPage from './pages/SignInPage.tsx'
+
 import ProductPitlover from './pages/ProductPitlover.tsx'
 
 const Routes=createBrowserRouter([{
