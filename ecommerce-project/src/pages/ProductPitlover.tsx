33ac5a45dc818fdda53c67013c0f8e7c30d66763
juanpit2 <juanpablo.ceballos05@gmail.com>
2025-11-07 16:01:11 -0500

    fix(ui): Resolve problems in ProductPitlover page

--- ecommerce-project/src/pages/ProductPitlover.tsx
+++ ecommerce-project/src/pages/ProductPitlover.tsx
@@ -10,9 +10,7 @@ export default function ProductsPage() {
       <Navbar />
       <div className="pt-48 px-4">
         <BannerProducts />
-        <h1 className="text-[32px] font-semibold text-[#0A0F1C] mt-14 mb-6 px-20">
-  Products
-</h1>
+      
         <ProductsGrid />
         <Benefits />
       </div>
